d2ffba243b38ae4bfdadfe74c1ad3d53f90adc7c
Andres6936 <andres6936@gmail.com>
2023-03-11 23:42:43 -0500

    Added Interfaces

--- sudoku/src/components/Cell.tsx
+++ sudoku/src/components/Cell.tsx
@@ -1,15 +1,26 @@
 import React, {useState} from "react";
 import {Coordinate} from "../types/Coordinate";
+import {UseBoard} from "../hooks/useBoard";
 
 interface Props {
+    board: UseBoard,
     coordinate: Coordinate
 }
 
 export function Cell(props: Props) {
+    const [cell, setCell] = useState(props.board.getCell({x: props.coordinate.x, y: props.coordinate.y}))
     const [notes, setNotes] = useState();
     const [number, setNumber] = useState<number>(0);
     const [solution, setSolution] = useState()
     const [isWrongNumber, setIsWrongNumber] = useState<boolean>(false);
+
+    const getValue = () => {
+        if (cell && cell.value) {
+            return cell.value
+        } else {
+            return ''
+        }
+    }
 
     const checkInput = () => {
         if (solution && solution !== number) {
@@ -24,7 +35,7 @@ export function Cell(props: Props) {
     return (
         <div onInput={() => checkInput()}
              className={"display:flex flex-grow:1 b:1px|solid|#CCC align-items:center justify-content:center"}>
-            <p className={"font:bold"}>{number === 0 ? "" : number}</p>
+            <p className={"font:bold"}>{getValue()}</p>
         </div>
     )
 }
--- sudoku/src/components/Zone.tsx
+++ sudoku/src/components/Zone.tsx
@@ -12,19 +12,19 @@ export function Zone(props: Props) {
     return (
         <div className={"display:flex flex:col flex-grow:1 m:0.5rem"}>
             <div className={"display:flex flex:row flex-grow:1"}>
-                <Cell coordinate={{x: 0, y: 0}}/>
-                <Cell coordinate={{x: 1, y: 0}}/>
-                <Cell coordinate={{x: 2, y: 0}}/>
+                <Cell board={props.board} coordinate={{x: 0, y: 0}}/>
+                <Cell board={props.board} coordinate={{x: 1, y: 0}}/>
+                <Cell board={props.board} coordinate={{x: 2, y: 0}}/>
             </div>
             <div className={"display:flex flex:row flex-grow:1"}>
-                <Cell coordinate={{x: 0, y: 1}}/>
-                <Cell coordinate={{x: 1, y: 1}}/>
-                <Cell coordinate={{x: 2, y: 1}}/>
+                <Cell board={props.board} coordinate={{x: 0, y: 1}}/>
+                <Cell board={props.board} coordinate={{x: 1, y: 1}}/>
+                <Cell board={props.board} coordinate={{x: 2, y: 1}}/>
             </div>
             <div className={"display:flex flex:row flex-grow:1"}>
-                <Cell coordinate={{x: 0, y: 2}}/>
-                <Cell coordinate={{x: 1, y: 2}}/>
-                <Cell coordinate={{x: 2, y: 2}}/>
+                <Cell board={props.board} coordinate={{x: 0, y: 2}}/>
+                <Cell board={props.board} coordinate={{x: 1, y: 2}}/>
+                <Cell board={props.board} coordinate={{x: 2, y: 2}}/>
             </div>
         </div>
     )
