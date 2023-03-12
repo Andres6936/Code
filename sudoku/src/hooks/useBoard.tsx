5ddd40b7f99b05f7bf2cffc90b9163d78fbad7c8
Andres6936 <andres6936@gmail.com>
2023-03-11 22:50:06 -0500

    Prepare Hooks

--- sudoku/src/hooks/useBoard.tsx
+++ sudoku/src/hooks/useBoard.tsx
@@ -1,13 +1,27 @@
 import {Coordinate} from "../types/Coordinate";
 
+function useDelimited(board: number[]) {
+    const zones = []
+    for (let index = 0; index < board.length; index++) {
+        zones.push({
+            x: 0,
+            y: 0,
+            value: board.at(index)
+        })
+    }
+    return zones;
+}
+
 export function useBoard(board: number[]) {
-    const getZone = ({x, y}: Coordinate): number[] => {
+    const zones = useDelimited(board);
 
+    const getZone = ({x, y}: Coordinate): number[] | null => {
+        return null;
     }
 
-    const getCell = ({x, y}: Coordinate): number => {
-
+    const getCell = ({x, y}: Coordinate): number | null => {
+        return null;
     }
 
-    return {getZone, getCell}
+    return {zones, getZone, getCell}
 }
