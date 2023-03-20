27c6e708404328b8084b0144deb32bdae1191d9e
Andres6936 <andres6936@gmail.com>
2023-03-19 22:41:49 -0500

    Added Focused Events

--- sudoku/src/components/InputCell.tsx
+++ sudoku/src/components/InputCell.tsx
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 import {Coordinate} from "../types/Coordinate";
-import {Cell} from "../types/Cell";
+import {Cell, isEqual} from "../types/Cell";
 import {useZone} from "../hooks/useZone";
 import {Optional} from "typescript-optional";
+import {SudokuContext} from "../context/SudokuContext";
 
 const EMPTY_PLACEHOLDER_VALUE = 0 as const;
 
@@ -12,12 +13,26 @@ interface Props {
 }
 
 export function InputCell(props: Props) {
+    const sudoku = useContext(SudokuContext);
     const cell = useZone(props.zone).getCell(props.coordinate);
 
     const [notes, setNotes] = useState();
     const [input, setInput] = useState<number>(0);
     const [solution, setSolution] = useState()
     const [isWrongNumber, setIsWrongNumber] = useState<boolean>(false);
+
+    const isFocusedByUser = () => {
+        const ownCell: Optional<Cell> = cell === null ? Optional.empty() : cell;
+        return isEqual(sudoku.currentCell, ownCell);
+    }
+
+    const getClassFocused = () => {
+        if (isFocusedByUser()) {
+            return "bg:red"
+        } else {
+            return ""
+        }
+    }
 
     const getPlaceholder = (): number | string => {
         if (cell) {
@@ -39,8 +54,8 @@ export function InputCell(props: Props) {
     }
 
     return (
-        <div onInput={() => checkInput()}
-             className={"display:flex flex-grow:1 b:1px|solid|#CCC align-items:center justify-content:center white-space:pre-wrap"}>
+        <div onClick={() => checkInput()}
+             className={"display:flex flex-grow:1 b:1px|solid|#CCC align-items:center justify-content:center white-space:pre-wrap " + getClassFocused()}>
             <p className={"font:bold"}>{getPlaceholder()}</p>
         </div>
     )
--- sudoku/src/types/Cell.ts
+++ sudoku/src/types/Cell.ts
@@ -1,9 +1,19 @@
+import {Optional} from "typescript-optional";
+
 export interface Cell {
     x: number,
     y: number,
     value: number
 }
 
-export function isEqual(origin: Cell, target: Cell) {
+export function isEqual(origin: Optional<Cell>, target: Optional<Cell>) {
+    if (origin.isEmpty() || target.isEmpty()) {
+        return false;
+    }
+
+    return isEqualOf(origin.get(), target.get());
+}
+
+function isEqualOf(origin: Cell, target: Cell) {
     return origin.x === target.x && origin.y === target.y && origin.value === target.value
 }
