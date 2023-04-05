e246d77c9a8b06ef717309753443a9f82bcf9bf2
Andres6936 <andres6936@gmail.com>
2023-04-04 21:24:13 -0500

    Select if not placeholder

--- sudoku/src/components/InputCell.tsx
+++ sudoku/src/components/InputCell.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {Coordinate} from "../types/Coordinate";
 import {Cell, isEqual} from "../types/Cell";
 import {useZone} from "../hooks/useZone";
@@ -21,14 +21,20 @@ export function InputCell(props: Props) {
     const [notes, setNotes] = useState();
     const [input, setInput] = useState<number>(0);
     const [solution, setSolution] = useState()
+    const [isPlaceholder, setIsPlaceholder] = useState<boolean>(false);
     const [isWrongNumber, setIsWrongNumber] = useState<boolean>(false);
+
+    // Avoid the use of cell as dependency, the value only is evaluated only once
+    useEffect(() => {
+        setIsPlaceholder(cell.value !== EMPTY_PLACEHOLDER_VALUE)
+    }, [])
 
     const isFocusedByUser = () => {
         return isEqual(sudoku.currentCell, Optional.ofNonNull(cell));
     }
 
     const getClassFocused = () => {
-        if (isFocusedByUser()) {
+        if (isFocusedByUser() && !isPlaceholder) {
             return "bg:sky-80"
         } else {
             return "bg:sky-88"
