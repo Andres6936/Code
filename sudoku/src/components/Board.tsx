b6f9f6a753de826ffb17177826d95f60148c7240
Andres6936 <andres6936@gmail.com>
2023-04-06 10:07:10 -0500

    Added Event Listener for Key

--- sudoku/src/components/Board.tsx
+++ sudoku/src/components/Board.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Zone} from "./Zone";
 
 export function Board() {
+    useEffect(() => {
+        addEventListener('keydown', onKeyDown);
+
+        return () => {
+            removeEventListener('keydown', onKeyDown);
+        }
+    }, [])
+
+    const onKeyDown = (event: KeyboardEvent) => {
+        console.log(event.key, 'Key')
+    }
+
     return (
         <div className="sudoku max-w:35rem my:2rem">
             <div className={"display:flex flex:row"}>
