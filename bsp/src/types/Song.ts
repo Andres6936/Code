cdd9c20885fe37c93dcc8c22bae274a29ca972e5
Andres6936 <andres6936@gmail.com>
2023-04-13 08:25:05 -0500

    Added Song

--- bsp/src/types/Song.ts
+++ bsp/src/types/Song.ts
@@ -2,7 +2,12 @@ export interface Song {
     title: string,
     date: string,
     author: string,
-    divide: string,
-    bpm: string,
+    divide: number,
+    bpm: number,
     comment: string,
+    cVol: number[],
+    wave: number[],
+    delay: number[],
+    sampleData: any[],
+    seq: any[]
 }
