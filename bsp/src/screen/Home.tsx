bfd1f4fb83d8da1b690e92963447d653e5aa2860
Andres6936 <andres6936@gmail.com>
2023-04-15 14:58:59 -0500

    Created Component

--- bsp/src/screen/Home.tsx
+++ bsp/src/screen/Home.tsx
@@ -14,6 +14,7 @@ import {useWavetablez2} from "../songs/Wavetablez2";
 import {useAutomation2} from "../songs/Automation2";
 import {useWavetablez3} from "../songs/Wavetablez3";
 import { useAutomation } from "../songs/Automation";
+import { Stop } from "../components/Stop";
 
 type TypeSong =
     "old" |
@@ -29,46 +30,7 @@ type TypeSong =
     "automation2" |
     "wavetablez3";
 
-type TypeState = "Stop" | "Resume";
 
-function Stop() {
-    const appContext = useContext<IAppContext>(AppContext)
-    const [currentState, setCurrentState] = useState<TypeState>('Stop')
-
-    const onSwithPlay = () => {
-        if (appContext.isPaused()) {
-            appContext.resume()
-            setCurrentState("Stop")
-        } else {
-            appContext.suspend()
-            setCurrentState("Resume")
-        }
-    }
-
-    const getIconByState = () => {
-        if (currentState === "Stop") {
-            return (
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-pause-fill color:blue-50" viewBox="0 0 16 16">
-                    <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"/>
-                </svg>
-            )
-        } else if (currentState === "Resume") {
-            return (
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-play-fill color:blue-50" viewBox="0 0 16 16">
-                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
-                </svg>
-            )
-        } else {
-            console.error("Current State Not Implemented")
-        }
-    }
-
-    return (
-        <div className={"display:flex align-items:center justify-content:center p:0.5rem position:absolute top:15 right:20 r:50% b:2px|solid|blue-50"} onClick={onSwithPlay}>
-            {getIconByState()}
-        </div>
-    )
-}
 
 function Description() {
     const appContext = useContext<IAppContext>(AppContext)
