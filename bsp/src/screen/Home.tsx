5519a1be03125d5362205e054d68769980a353f4
Andres6936 <andres6936@gmail.com>
2023-04-15 14:26:21 -0500

    Added Styles

--- bsp/src/screen/Home.tsx
+++ bsp/src/screen/Home.tsx
@@ -157,7 +157,18 @@ export function Home() {
                 </div>
             </div>
 
-            <h1 className={"font-family:bahnschrift  font-size:2.5rem color:white uppercase"}>Your Playlist</h1>
+            <h1 className={"font-family:bahnschrift font-size:2.5rem color:white uppercase"}>Your Playlist</h1>
+
+            <div className={"display:flex flex:row align-items:center justify-content:space-between bb:1px|solid|#CCC"}>
+                <div className={"color:white opacity:0.5 font-size:1.2rem font-family:bahnschrift"}>
+                    <p className={"p:0 mb:0.4rem"}>Search ...</p>
+                </div>
+                <div className={"color:white opacity:0.5"}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                </div>
+            </div>
 
             <Stop/>
             <ListSong/>
