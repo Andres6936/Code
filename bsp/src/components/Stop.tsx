f24f99f9b8acd36c960d0d6a05ddeaf739ca6868
Andres6936 <andres6936@gmail.com>
2023-04-15 21:10:26 -0500

    Moved Component

--- bsp/src/components/Stop.tsx
+++ bsp/src/components/Stop.tsx
@@ -1,5 +1,6 @@
-import { useContext, useState } from "react";
-import {AppContext, IAppContext } from "../context/AppContext";
+import {useContext, useState} from "react";
+import {AppContext, IAppContext} from "../context/AppContext";
+import {IconPlay} from "./icons/IconPlay";
 
 type TypeState = "Stop" | "Resume";
 
@@ -20,24 +21,26 @@ export function Stop() {
     const getIconByState = () => {
         if (currentState === "Stop") {
             return (
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-pause-fill color:blue-50" viewBox="0 0 16 16">
-                    <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                     className="bi bi-pause-fill color:blue-50" viewBox="0 0 16 16">
+                    <path
+                        d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"/>
                 </svg>
-                )
+            )
         } else if (currentState === "Resume") {
             return (
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-play-fill color:blue-50" viewBox="0 0 16 16">
-                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
-                </svg>
-                )
+                <IconPlay/>
+            )
         } else {
             console.error("Current State Not Implemented")
         }
     }
 
     return (
-        <div className={"display:flex align-items:center justify-content:center p:0.5rem position:absolute top:15 right:20 r:50% b:2px|solid|blue-50"} onClick={onSwithPlay}>
+        <div
+            className={"display:flex align-items:center justify-content:center p:0.5rem position:absolute top:15 right:20 r:50% b:2px|solid|blue-50"}
+            onClick={onSwithPlay}>
             {getIconByState()}
         </div>
-        )
+    )
 }
